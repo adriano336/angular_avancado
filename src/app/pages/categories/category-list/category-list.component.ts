ede27c499e51ead9bdf0683d685efd9cbd9acee7
Adriano Chagas <adrianochagas336@gmail.com>
2025-05-31 16:49:29 -0300

    componente de lista de categoria

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.scss'
 })
 export class CategoryListComponent {
+
+
+  excluirCategoria(id : number ){
+    alert(id);
+  }
 
 }
